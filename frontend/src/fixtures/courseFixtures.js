45228174baba9c97c331b1d7be2af430e3ca6972
Garrett Hu <garretthu@ucsb.edu>
2023-11-21 14:40:51 -0800

    added fixtures for courses

---
+++ frontend/src/fixtures/courseFixtures.js
@@ -0,0 +1,45 @@
+const courseFixtures = {
+    oneCourse:
+    [
+        {
+            "id": 1,
+            "name": "CMPSC156",
+            "school": "UCSB",
+            "term": "Fall",
+            "start": "2023-09-27T14:00:00",
+            "end": "2023-12-12T15:15:00",
+            "githubOrg": "ucsb-cs156-f23"
+        }
+    ],
+    threeCourses: [
+        {
+            "id": 2,
+            "name": "CMPSC16",
+            "school": "UCSB",
+            "term": "Fall",
+            "start": "2021-09-24T14:00:00",
+            "end": "2021-12-12T15:15:00",
+            "githubOrg": "ucsb-cs16-f21"
+        },
+        {
+            "id": 3,
+            "name": "PSTAT10",
+            "school": "UCSB",
+            "term": "Spring",
+            "start": "2023-03-24T11:00:00",
+            "end": "2023-06-13T11:50:00",
+            "githubOrg": "ucsb-pstat8-s23"
+        },
+        {
+            "id": 4,
+            "name": "CE190I",
+            "school": "UCLA",
+            "term": "Winter",
+            "start": "2023-01-03T08:00:00",
+            "end": "2023-03-15T09:15:00",
+            "githubOrg": "ucla-ce190i-w23"
+        }
+    ]
+};
+
+export { courseFixtures };
