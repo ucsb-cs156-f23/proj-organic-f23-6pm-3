4e49bb84c89bec0f6cbc76f79e210aae6374dd9e
bella-ward <108088761+bella-ward@users.noreply.github.com>
2023-11-26 03:20:27 -0800

    bw - copied branch before push, now this one

--- src/test/java/edu/ucsb/cs156/organic/controllers/JobsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/organic/controllers/JobsControllerTests.java
@@ -209,7 +209,7 @@ public class JobsControllerTests extends ControllerTestCase {
 
                 // act
                 MvcResult response = mockMvc
-                                .perform(post("/api/jobs/launch/testjob?fail=true&sleepMs=3000").with(csrf()))
+                                .perform(post("/api/jobs/launch/testjob?fail=true&sleepMs=4000").with(csrf()))
                                 .andExpect(status().isOk()).andReturn();
 
                 String responseString = response.getResponse().getContentAsString();
