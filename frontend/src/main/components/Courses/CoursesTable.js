ed01d8fa268ad9e9225f318e213773d8d10270a2
Garrett Hu <garretthu@ucsb.edu>
2023-11-25 20:52:14 -0800

    added table + tests + stories for courses

---
+++ frontend/src/main/components/Courses/CoursesTable.js
@@ -0,0 +1,70 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/courseUtils"
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+
+export default function CoursesTable({ courses, currentUser }) {
+    
+    const navigate = useNavigate();
+
+    const editCallback = (cell) => {
+        navigate(`/courses/edit/${cell.row.values.id}`)
+    }
+
+    // Stryker disable all : hard to test for query caching
+
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/courses/all"]
+    );
+    // Stryker restore all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this    
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+    const columns = [
+        {
+            Header: 'id',
+            accessor: 'id',
+        },
+        {
+            Header: 'Name',
+            accessor: 'name',
+        },
+        {
+            Header: 'School',
+            accessor: 'school',
+        },
+        {
+            Header: 'Term',
+            accessor: 'term',
+        },
+        {
+            Header: 'Start',
+            id: 'start',
+        },
+        {
+            Header: 'End',
+            id: 'end',
+        },
+        {
+            Header: 'GithubOrg',
+            accessor: 'githubOrg',
+        }
+    ];
+
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        columns.push(ButtonColumn("Edit", "primary", editCallback, "CoursesTable"));
+        columns.push(ButtonColumn("Delete", "danger", deleteCallback, "CoursesTable"));
+    } 
+    
+    return <OurTable
+        data={courses}
+        columns={columns}
+        testid={"CoursesTable"} />;
+};
