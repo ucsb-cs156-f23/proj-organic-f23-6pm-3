1f15871709b0a40b68642e9daac62f89efca0e8f
Yicong Yan <yicongyan@ucsb.edu>
2023-11-28 15:54:09 -0800

    fix job controller again

--- src/test/java/edu/ucsb/cs156/organic/controllers/JobsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/organic/controllers/JobsControllerTests.java
@@ -218,7 +218,7 @@ public class JobsControllerTests extends ControllerTestCase {
                 assertEquals("running", jobReturned.getStatus());
 
 
-                await().atMost(50, SECONDS)
+                await().atMost(200, SECONDS)
 
                 .untilAsserted(() -> {
                         verify(jobsRepository, atLeast(1)).save(jobCaptor.capture());                        
