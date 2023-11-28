f124ddf72b343e1804e5f05c91ca2b8f4cdb4d0f
Garrett Hu <garretthu@ucsb.edu>
2023-11-28 00:27:52 -0800

    removed files from different branch

---
+++ frontend/src/tests/pages/Courses/CoursesEditPage.test.js
@@ -0,0 +1,203 @@
+import { fireEvent, render, waitFor, screen } from "@testing-library/react";
+import CoursesEditPage from "main/pages/Courses/CoursesEditPage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import mockConsole from "jest-mock-console";
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        useParams: () => ({
+            id: 17
+        }),
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
+
+describe("CoursesEditPage tests", () => {
+
+    describe("when the backend doesn't return data", () => {
+
+        const axiosMock = new AxiosMockAdapter(axios);
+
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/courses", { params: { id: 17 } }).timeout();
+        });
+
+        const queryClient = new QueryClient();
+        test("renders header but table is not present", async () => {
+
+            const restoreConsole = mockConsole();
+
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <CoursesEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+            await screen.findByText("Edit Course");
+            expect(screen.queryByTestId("CourseForm-name")).not.toBeInTheDocument(); // TODO: plural?
+            restoreConsole();
+        });
+    });
+
+    describe("tests where backend is working normally", () => {
+
+        const axiosMock = new AxiosMockAdapter(axios);
+
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/courses", { params: { id: 17 } }).reply(200, {
+                id: 17,
+                name: "CMPSC 156",
+                school: "UCSB",
+                term: "F23",
+                start: "2023-09-27T14:00",
+                end: "2023-12-12T15:15",
+                githubOrg: "ucsb-cs156-f23"
+            });
+            axiosMock.onPut('/api/courses').reply(200, {
+                id: "17",
+                name: "CMPSC 111",
+                school: "UCLA",
+                term: "W24",
+                start: "2024-01-01T09:00",
+                end: "2024-03-15T10:15",
+                githubOrg: "ucla-cs111-w24"
+            });
+        });
+
+        const queryClient = new QueryClient();
+        test("renders without crashing", () => {
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <CoursesEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+        });
+
+        test("Is populated with the data provided", async () => {
+
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <CoursesEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+
+            await screen.findByTestId("CourseForm-name");
+
+            const idField = screen.getByTestId("CourseForm-id");
+            const nameField = screen.getByTestId("CourseForm-name");
+            const schoolField = screen.getByTestId("CourseForm-school");
+            const termField = screen.getByTestId("CourseForm-term");
+            const startField = screen.getByTestId("CourseForm-start");
+            const endField = screen.getByTestId("CourseForm-end");
+            const githubOrgField = screen.getByTestId("CourseForm-githubOrg");
+            const submitButton = screen.getByTestId("CourseForm-submit");
+
+            expect(idField).toHaveValue("17");
+            expect(nameField).toHaveValue("CMPSC 156");
+            expect(schoolField).toHaveValue("UCSB");
+            expect(termField).toHaveValue("F23");
+            expect(startField).toHaveValue("2023-09-27T14:00");
+            expect(endField).toHaveValue("2023-12-12T15:15");
+            expect(githubOrgField).toHaveValue("ucsb-cs156-f23");
+            expect(submitButton).toBeInTheDocument();
+        });
+
+        test("Changes when you click Update", async () => {
+
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <CoursesEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+
+            await screen.findByTestId("CourseForm-name");
+
+            const idField = screen.getByTestId("CourseForm-id");
+            const nameField = screen.getByTestId("CourseForm-name");
+            const schoolField = screen.getByTestId("CourseForm-school");
+            const termField = screen.getByTestId("CourseForm-term");
+            const startField = screen.getByTestId("CourseForm-start");
+            const endField = screen.getByTestId("CourseForm-end");
+            const githubOrgField = screen.getByTestId("CourseForm-githubOrg");
+            const submitButton = screen.getByTestId("CourseForm-submit");
+
+            expect(idField).toHaveValue("17");
+            expect(nameField).toHaveValue("CMPSC 156");
+            expect(schoolField).toHaveValue("UCSB");
+            expect(termField).toHaveValue("F23");
+            expect(startField).toHaveValue("2023-09-27T14:00");
+            expect(endField).toHaveValue("2023-12-12T15:15");
+            expect(githubOrgField).toHaveValue("ucsb-cs156-f23");
+            expect(submitButton).toBeInTheDocument();
+
+            fireEvent.change(nameField, { target: { value: 'CMPSC 111' } })
+            fireEvent.change(schoolField, { target: { value: 'UCLA' } })
+            fireEvent.change(termField, { target: { value: 'W24' } })
+            fireEvent.change(startField, { target: { value: '2024-01-01T09:00' } })
+            fireEvent.change(endField, { target: { value: '2024-03-15T10:15' } })
+            fireEvent.change(githubOrgField, { target: { value: 'ucla-cs111-w24' } })
+
+            fireEvent.click(submitButton);
+
+            // await waitFor(() => expect(mockToast).toBeCalled()); //,{timeout:10000}
+            await waitFor(() => expect(mockToast).toHaveBeenCalled());
+            expect(mockToast).toBeCalledWith("Course Updated - id: 17 githubOrg: ucla-cs111-w24");
+            expect(mockNavigate).toBeCalledWith({ "to": "/courses" });
+
+            expect(nameField).toHaveValue("CMPSC 111");
+
+            // await new Promise(r => setTimeout(r, 2000));
+
+            expect(axiosMock.history.put.length).toBe(1); // times called
+            expect(axiosMock.history.put[0].params).toEqual({ id: 17 });
+            expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
+                name: "CMPSC 111",
+                school: "UCLA",
+                term: "W24",
+                start: "2024-01-01T09:00",
+                end: "2024-03-15T10:15",
+                githubOrg: "ucla-cs111-w24"
+            })); // posted object
+
+        });
+
+
+    });
+
+});
